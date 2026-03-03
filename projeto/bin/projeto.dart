abe4c683354dd77862d2623c8adf323375775129
zLuuuck <lucas.toterol.rodrigues@gmail.com>
2026-03-03 18:46:16 -0300

    atualização

--- projeto/bin/projeto.dart
+++ projeto/bin/projeto.dart
@@ -1,3 +1,15 @@
-void main(List<String> arguments) {
-  print('Hello world!');
+void main() {
+
+    double teste;
+
+    teste =1.0;
+
+    String testeNome = 'Teste';
+
+    var preco = 2.99;
+
+    print(teste);
+
+
+    print("O valor da sua banana deu: $preco. Variavel nome: $testeNome");
 }
