f6cb9deed154b63eb51c652fb0a18edc770d3d3a
Caio <caio.f.arze@gmail.com>
2026-04-01 20:02:26 -0300

    exit ex1

--- projeto/estudo-dirigido-1.1/lib/questao02/main.dart
+++ projeto/estudo-dirigido-1.1/lib/questao02/main.dart
@@ -1,8 +1,8 @@
 import 'relogio.dart';
 
 void main() {
-  var hora1 = Relogio(19, 24, 34);
-  var hora2 = Relogio(15, 25, 30);
+  var hora1 = Relogio(18, 34, 34);
+  var hora2 = Relogio(15, 3, 30);
   var hora3 = Relogio(1, 2, 3);
 
   hora1.exibirHorario();
--- projeto/estudo-dirigido-1.1/lib/questao02/relogio.dart
+++ projeto/estudo-dirigido-1.1/lib/questao02/relogio.dart
@@ -1,17 +1,22 @@
+import 'dart:io';
+
 class Relogio {
   int _hora;
   int _minuto;
   int _segundo;
 
   Relogio(this._hora, this._minuto, this._segundo) {
-    if (0 > _hora || _hora > 23) {
+    if (_hora < 0 || _hora > 23) {
       print("Hora deve estar entre 0 e 23!");
+      exit(1);
     }
-    if (0 > _minuto || _minuto > 59) {
+    if (_minuto < 0 || _minuto > 59) {
       print("Minuto deve estar entre 0-59!");
+      exit(1);
     }
-    if (0 > _segundo || _segundo > 59) {
-      print("Minuto deve estar entre 0-59!");
+    if (_segundo < 0 || _segundo > 59) {
+      print("Segundo deve estar entre 0-59!");
+      exit(1);
     }
   }
 
