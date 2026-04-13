a762801b405d75fe9023a81a494fc2cefdba281f
zLuuuck <lucas.toterol.rodrigues@gmail.com>
2026-04-13 08:46:22 -0300

    atualizando repo

--- projeto/estudo-dirigido-1.1/lib/questao20/pokemon.dart
+++ projeto/estudo-dirigido-1.1/lib/questao20/pokemon.dart
@@ -1,29 +1,67 @@
-class Pokemon {
-  int numero;
-  String nome;
-  String tipo;
-  int nivel;
-  int hp;
+import 'dart:io';
 
-  Pokemon(this.numero, this.nome, this.tipo, this.nivel, this.hp) {
-    if (numero <= 0) {
-      numero = 1;
+class Pokemon {
+  int _numero;
+  String _nome;
+  String _tipo;
+  int _nivel;
+  int _hp;
+
+  Pokemon(this._numero, this._nome, this._tipo, this._nivel, this._hp) {
+    if (_numero <= 0) {
+      print("O número do seu pokemon deve ser maior que 0");
+      exit(1);
     }
-    if (nome.isEmpty) {
-      nome = "Pokemon";
-      }
-    if (tipo.isEmpty) {
-      tipo = "Normal";
+    if (_nome.isEmpty) {
+      
     }
-    if (nivel < 1 || nivel > 100) {
-      nivel = 1;
+    if (_tipo.isEmpty) {
+      _tipo = "Normal";
     }
-    if (hp <= 0) {
-      hp = 1;
+    if (_nivel < 1 || _nivel > 100) {
+      _nivel = 1;
+    }
+    if (_hp <= 0) {
+      _hp = 1;
     }
   }
 
+// setter e getter do numero
+  set numero(numero) {
+    _numero = numero;
+  }
+
+  int get numero => _numero;
+
+// setter e getter de nome
+  set nome(nome) {
+    _nome = nome;
+  }
+
+  String get nome => _nome;
+
+//setter e gett de tipo
+  set tipo(tipo) {
+    _tipo = tipo;
+  }
+
+  String get tipo => _tipo;
+
+// setter e getter de nivel
+  set nivel(nivel) {
+    _nivel = nivel;
+  }
+
+  int get nivel => _nivel;
+
+// setter e getter de hp
+  set hp(hp) {
+    _hp = hp;
+  }
+
+  int get hp => _hp;
+
   void exibir() {
-    print("$nome (Nv: $nivel, HP: $hp, Tipo: $tipo)");
+    print("$_nome (Nv: $_nivel, _HP: $_hp, _Tipo: $_tipo)");
   }
 }
