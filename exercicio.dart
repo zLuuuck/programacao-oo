8a400870960459a729c726a2e0dfcb5bc29a0cfc
zLuuuck <lucas.toterol.rodrigues@gmail.com>
2026-05-20 21:44:56 -0300

    fnsdnoifnasdoifdas

---
+++ exercicio.dart
@@ -0,0 +1,16 @@
+// 1) crie um classe chamada pilha que simula o comportamento de uma pilha genérica (Pilha<T>), onde T é o tipo dos elementos armazenados na pilha. A classe deve ter os seguintes métodos:
+// a) Crie a pilha para ser gerenérica <T>
+// b) sobrescreva os operadores para realizar as seguintes operações:
+// Pilha += item    -> empilha um item à pilha
+// Pilha --    -> desempilha um item da pilha
+// pilha[i]   -> retorna o item na posição i da pilha
+
+Class Pilha<T> {
+  final List<T> _itens = [];
+
+  static const int _capacidadeMaxima = 10;
+
+  
+
+}
+
